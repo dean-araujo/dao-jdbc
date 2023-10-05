1c5054aa34f6a89344c0b108eab84270c0f2a4a2
Dean Araujo <deantarik4@gmail.com>
2023-10-05 15:52:24 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		//Test
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+		
+		SellerDao sellerDao = DaoFactory.CreateSellerDao();
 		
 		System.out.println(obj);
 		System.out.println(seller);
