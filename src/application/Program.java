b6f3e92f1a9a8b0e87941f034c0932d48f63605e
Dean Araujo <deantarik4@gmail.com>
2023-10-05 15:40:20 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		//Test
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
